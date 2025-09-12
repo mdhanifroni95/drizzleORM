d71a26bf6465ebbe840a9917172bc7ceed574892
mdhanifroni95 <eng.hanif.roni63@gmail.com>
2025-09-12 18:39:29 +0600

    feat: add profile_info table with metadata and user_id reference

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,3 +1,5 @@
+import { integer } from "drizzle-orm/pg-core";
+import { jsonb } from "drizzle-orm/pg-core";
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
@@ -5,4 +7,10 @@ export const users = pgTable("users", {
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   password: text("password").notNull(),
+});
+
+export const profileInfo = pgTable("profile_info", {
+  id: serial("id").primaryKey(),
+  metadata: jsonb("metadata"),
+  user_id: integer("user_id").references(() => users.id),
 });
