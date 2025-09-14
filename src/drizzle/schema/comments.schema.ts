f1ab02f299a7c89561fb72b4f2c3a526d9106e2e
mdhanifroni95 <eng.hanif.roni63@gmail.com>
2025-09-14 18:28:16 +0600

    feat: add comments and groups schema with relationships to users

--- src/drizzle/schema/comments.schema.ts
+++ src/drizzle/schema/comments.schema.ts
@@ -1,9 +1,10 @@
 import { pgTable, text, integer, serial } from "drizzle-orm/pg-core";
 import { users } from "./users.schema";
+import { posts } from "./posts.schema";
 
-export const posts = pgTable("posts", {
+export const comments = pgTable("comments", {
   id: serial("id").primaryKey(),
-  title: text("title").notNull(),
-  content: text("content").notNull(),
+  comments: text("comments").notNull(),
   author_id: integer("author_id").references(() => users.id),
+  post_id: integer("post_id").references(() => posts.id),
 });
---
+++ src/drizzle/schema/groups.schema.ts
@@ -0,0 +1,20 @@
+import { pgTable, text, serial, integer } from "drizzle-orm/pg-core";
+import { users } from "./users.schema";
+import { primaryKey } from "drizzle-orm/pg-core";
+
+export const groups = pgTable("groups", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+});
+
+//This the joint table
+export const usersToGroups = pgTable(
+  "users_to_groups",
+  {
+    user_id: integer("id").references(() => users.id),
+    group_id: integer("group_id").references(() => groups.id),
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.user_id, table.group_id] }),
+  }),
+);
